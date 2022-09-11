8cd50b9b6690afe05ee31c322066f99da38e41be
Ben Okello Mwaka <mwakaben@gmail.com>
2022-09-11 23:41:59 +0300

    homepage card views

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -13,48 +13,85 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(
-              height: size.height * .3,
-              decoration: BoxDecoration(
-                image: DecorationImage(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
                   alignment: Alignment.topCenter,
-                  image: AssetImage('assets/images/homebanner.png')
-                ),
-              ),
+                  image: AssetImage('assets/images/homebanner.png')),
             ),
-
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 64,
-                      child: Row(
-                        children: <Widget>[
-                          CircleAvatar(
-                            radius: 32,
-                            backgroundImage: AssetImage('assets/images/profile.png'),
-                          ),
-                          Column(
-                            children:<Widget> [
-                              Text('Ben OKELLO MWAKA'),
-                              Text('ES-01-1244835')
-                            ],
-                          )
-                        ],
-                      ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 64,
+                    margin: EdgeInsets.only(bottom: 40),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundImage:
+                              AssetImage('assets/images/profile.png'),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Ben OKELLO MWAKA',
+                              style: TextStyle(
+                                  fontFamily: 'Century Gothic',
+                                  color: Colors.white,
+                                  fontSize: 16),
+                            ),
+                            Text(
+                              'ES-01-1244835',
+                              style: TextStyle(
+                                  fontFamily: 'SUNA Arial',
+                                  color: Colors.white,
+                                  fontSize: 11),
+                            )
+                          ],
+                        )
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  
+                  Expanded(
+                    child: GridView.count(
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        primary: false,
+                        children: <Widget>[
+                          Card(
+                            elevation:4,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SvgPicture.network(
+                                    'https://www.svgrepo.com/show/327459/people.svg', height:128,),
+                                Text('Saving Groups')
+                              ],
+                            ),
+                          ),
+                        ],
+                        crossAxisCount: 2),
+                  ),
+                ],
               ),
             ),
-          
-          ],
-        ),
-      
+          ),
+        ],
+      ),
     );
   }
 }
